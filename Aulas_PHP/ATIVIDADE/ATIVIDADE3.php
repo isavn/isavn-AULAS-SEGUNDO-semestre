53a1202511f14669787e9b90f6f966e0df1de5e2
isavn <isabellavieiraneves@gmail.com>
2026-06-17 22:20:45 -0300

    atividades dia 17/06

--- Aulas_PHP/ATIVIDADE/ATIVIDADE3.php
+++ Aulas_PHP/ATIVIDADE/ATIVIDADE3.php
@@ -1,3 +1,16 @@
 // criar um array de 10 e 20 com for 
 fazer loop em cima do array criado dinamicamente 
 imprimindo apenas numero impares
+
+<?php
+$numeros = [];
+for ($i = 10; $i<= 20; $i++){
+    $numeros[] = $i;
+$total = count($numeros);
+for ($i = 0; $i < $total; $i++){
+    if ($numeros[$i] % 2 != 0){
+echo $numeros[$i]." ";
+    }
+}
+}
+?>
